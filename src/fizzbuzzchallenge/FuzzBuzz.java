0331bf5e2b1ddeb15d4bae35f44075391808db28
Adriano <adriano.henrique.eng@gmail.com>
2021-09-10 23:55:50 -0300

    Initial commit

---
+++ src/fizzbuzzchallenge/FuzzBuzz.java
@@ -0,0 +1,11 @@
+package fizzbuzzchallenge;
+
+public class FuzzBuzz {
+    /*FizzBuzz
+    - Write a program that prompts the user for a number and prints that number.
+    - If the number is divisible by three, then print "Fizz" instead of the number.
+    - If the number is divisible by five, then print "Buzz" instead of the number.
+    - If the number is divisible by both three and five, then print "FizzBuzz" instead of the number.
+    */
+    
+}
